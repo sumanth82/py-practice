404bc152bcae30f1daf188bd808342bc976d15f9
sumanth82 <sumanth82@gmail.com>
2020-05-02 11:43:16 -0400

    practice-python code

--- practicepython/exercise-1.py
+++ practicepython/exercise-1.py
@@ -1,5 +1,8 @@
 #  http://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+import datetime
+from datetime import date
 
 user_input = input("Enter your age : ")
 
 year_when_user_turns_100_years =  
+
