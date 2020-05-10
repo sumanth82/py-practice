eae5289cf4206af641c91f67c5100f17a2ac10f4
sumanth82 <sumanth82@gmail.com>
2020-05-10 00:38:47 -0400

    Changes

---
+++ python-workbook-practice/3.py
@@ -0,0 +1,9 @@
+# Question: Executing the code will throw an error. Can you explain why?
+
+a = 1
+b = 2
+print(a == b)
+print(b == c)
+
+# ANS: NameError: name 'c' is not defined
+
---
+++ python-workbook-practice/6.py
@@ -0,0 +1,14 @@
+# Question: Please complete the script so that it prints out a list slice containing 
+# items d , e , and f .
+
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# Expected output: 
+
+# ['d', 'e', 'f'] 
+
+SOL:
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[])
+
+
