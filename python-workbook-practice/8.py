a83c8e95b57380563b362f4d65008620c43e1a99
sumanth82 <sumanth82@gmail.com>
2020-05-13 22:50:48 -0400

    Changes

---
+++ python-workbook-practice/8.py
@@ -0,0 +1,33 @@
+# Question: Complete the script so that it prints out letter i  
+# using negative indexing.
+
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# Expected output: 
+
+# i 
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+print(letters[-1])
+
+#O/P: j // Last letter
+end = (letters[::-1])
+print(end)
+
+# O/P: ['j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
+
+print(end[1])
+
+##### OR ####
+
+print(letters[-2])
+
+# O/P: i
+
+
+
+
+# O/P: i
+
+
+
