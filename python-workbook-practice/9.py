619a00e2e3bd4805afd177b7ff70b125a2099d49
sumanth82 <sumanth82@gmail.com>
2020-05-13 23:06:06 -0400

    Changes

---
+++ python-workbook-practice/9.py
@@ -0,0 +1,17 @@
+# Question: Complete the script so that it prints out a list slice 
+# containing the last three items of list letters .
+
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# Expected output:
+
+# ['h', 'i', 'j'] 
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+print(letters[-3:])
+
+# O/P: ['h', 'i', 'j']
+
+
+
+
