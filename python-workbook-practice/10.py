8bfbfcd491f748e7edc331651f11576c9278e522
sumanth82 <sumanth82@gmail.com>
2020-05-13 23:11:14 -0400

    Changes

---
+++ python-workbook-practice/10.py
@@ -0,0 +1,13 @@
+# Question: Complete the script so that it prints out a list slice containing 
+# letters a, c, e, g, and i. 
+
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# Expected output: 
+
+# ['a', 'c', 'e', 'g', 'i'] 
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[0::2])
+
+# O/P: ['a', 'c', 'e', 'g', 'i']
+
