960d5f567169c783cb4250d294894be7d685080f
sumanth82 <sumanth82@gmail.com>
2020-05-18 23:54:14 -0400

    11.py

--- python-workbook-practice/11-20/11.py
+++ python-workbook-practice/11-20/11.py
@@ -1,12 +1,13 @@
 # Create a list from 1 to 20
 
-i = 1
+empty_list = []
 
-for i in range(20):
-    print(i)
-    print(type(i))
-    
-# print(list(i))
+for i in range(1, 21):
+    empty_list.append(i)
+
+print(empty_list)
+
+# O/P: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 
 
