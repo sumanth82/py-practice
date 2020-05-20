7a9a43872595ba603bebc7419c63ba8049b48cd2
sumanth82 <sumanth82@gmail.com>
2020-05-19 20:42:41 -0400

    Changes

--- python-workbook-practice/11-20/13.py
+++ python-workbook-practice/11-20/13.py
@@ -7,9 +7,21 @@
 #['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']  
 
 my_range = range(1, 21)
-print(list(my_range))
 
-print()
+print(my_range) # O/P: range(1, 21)
+print(str(my_range)) # O/P: range(1, 21)
+
+# Notes: str() function converts numbers to strings 
+# map() is a built-in function used to convert list to strings
+
+map(str, my_range) # Takes an empty str function and iterates using the range
+print(map(str, my_range))
+
+# O/P: <map object at 0x109222bd0>
+
+print(list(map(str, my_range)))
+
+# O/P: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
 
 
 
