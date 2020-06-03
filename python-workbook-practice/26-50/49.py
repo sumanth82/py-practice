e3c75eb32c5da8449cdf8480c5be9b13bfa43a7a
sumanth82 <sumanth82@gmail.com>
2020-06-02 22:48:57 -0400

    26 and upwards

---
+++ python-workbook-practice/26-50/49.py
@@ -0,0 +1,11 @@
+# Question: The code is supposed to get some input from the user, but instead it produces an error. Please try to understand the error and then fix it.
+# pass = input("Please enter your password: ")
+
+pass1=input("Please enter your password: ")
+
+for letter in "Hello":
+    if letter == "e":
+        print(letter)
+    
+
+
