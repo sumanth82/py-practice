c1d39162d23714518c238396ba1d766beefc5733
sumanth82 <sumanth82@gmail.com>
2020-06-06 14:42:54 -0400

    31-34

---
+++ python-workbook-practice/26-50/32.py
@@ -0,0 +1,14 @@
+# Question:  What will the following script output? Please try to do this mentally if you can.
+
+#c = 1
+#def foo():
+#    return c
+#c = 3
+#print(foo())
+
+c = 1
+def foo():
+    return c
+c = 3
+print(foo())
+
