8c2bce7e10cd69a5cd3e3fb48c88c02a2d0582ba
sumanth82 <sumanth82@gmail.com>
2020-06-13 11:25:30 -0400

    Updates to 39

---
+++ python-workbook-practice/26-50/39.py
@@ -0,0 +1,14 @@
+# Question: The script is supposed to output the cosine of angle 1 radian, but instead it is throwing an error. 
+# Please fix the code so that it prints out the expected output.
+
+import math
+print(math.cosine(1)) # Incorrect
+
+print(math.cos(1)) # correct one is this 
+
+#Expected output:
+#0.5403023058681397    
+
+#>>> import math
+#>>> dir(math)
+# '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'ceil', 'copysign', 'cos', 'cosh', 'degrees', 'e', 'erf', 'erfc', 'exp', 'expm1', 'fabs', 'factorial', 'floor', 'fmod', 'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'modf', 'nan', 'pi', 'pow', 'radians', 'remainder', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'tau', 'trunc']
