a767a004a727410025b528a2c8625f5621ef9c82
sumanth82 <sumanth82@gmail.com>
2020-06-18 22:05:08 -0400

    Updates

---
+++ python-workbook-practice/26-50/45-open-method-in-dir.py
@@ -0,0 +1,13 @@
+#Question: Please create a script that generates 26 text files named a.txt, b.txt, 
+# and so on up to z.txt. Each file should contain a letter reflecting its filename. 
+# So, a.txt will contain letter a, b.txt will contain letter b and so on.
+
+import string
+import os
+
+if not os.path.exists('letters'):
+    os.makedirs('letters')
+
+for letter in string.ascii_lowercase:
+    with open('letters/' + letter + '.txt', 'w') as file:
+        file.write(letter + '\n')
