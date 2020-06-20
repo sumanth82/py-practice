fc31571ff560b3525b20511db32c8047a50611c6
sumanth82 <sumanth82@gmail.com>
2020-06-20 14:07:15 -0400

    Updates

--- python-workbook-practice/26-50/44-letters-three-by-three-in-file.py
+++ python-workbook-practice/26-50/44-letters-three-by-three-in-file.py
@@ -14,5 +14,24 @@ with open('three-by-three.txt', 'w+') as files:
         files.write(letter1 + letter2 + letter3 + '\n')
 
 
+######### RIGHT SOLUTION #################
+
+import string
+ 
+letters = string.ascii_lowercase + " "
+ 
+slice1 = letters[0::3]
+slice2 = letters[1::3]
+slice3 = letters[2::3]
+ 
+with open("letters.txt", "w") as file:
+    for s1, s2, s3 in zip(slice1, slice2, slice3):
+        file.write(s1 + s2 + s3 + "\n")
+
+
+## Another difference is that we are adding one white spaces to ascii_lowercase  so that we get a string of 27 items as opposed to 26 that ascii_lowercase  provides. By slicingascii_lowercase we would get two slices with a length of 9 and one slice with a length of 8. 
+# That means the iteration would stop at the shortest slice so letters y and z wouldn't be written in the file. That's why we add a white space so we get three slices each with a length of 9. That means we get 9 iterations and not 8 as we would get with the original ascii_lowercase  string. The 9th iteration would write letters y and z in the file.
+
+
 
 
