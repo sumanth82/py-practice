cdc6bae669bfef8b8f44bc494d4af65be9116914
sumanth82 <sumanth82@gmail.com>
2020-06-22 04:42:59 -0400

    Updates

---
+++ python-workbook-practice/51-75/54-modify-multilevel-dict.py
@@ -0,0 +1,32 @@
+#Question: Please update the dictionary by changing the last name of the second employee from Smith to Smooth or to whatever takes your fancy.
+
+#d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+#                {"firstName": "Anna", "lastName": "Smith"},
+#                {"firstName": "Peter", "lastName": "Jones"}],
+#"owners":[{"firstName": "Jack", "lastName": "Petter"},
+#          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+#Expected output: 
+
+#d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+#                {"firstName": "Anna", "lastName": "Smooth"},
+#                {"firstName": "Peter", "lastName": "Jones"}],
+#"owners":[{"firstName": "Jack", "lastName": "Petter"},
+#          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+for v in d.values():
+    if (d['employees'][1]['lastName']) == 'Smith':
+        (d['employees'][1]['lastName']) = 'Smooth'
+    print(d['employees'][1]['lastName'])    # O/P: Smooth
+
+print(d)
+
+# {'employees': [{'firstName': 'John', 'lastName': 'Doe'}, {'firstName': 'Anna', 'lastName': 'Smooth'}, {'firstName': 'Peter', 'lastName': 'Jones'}], 'owners': [{'firstName': 'Jack', 'lastName': 'Petter'}, {'firstName': 'Jessy', 'lastName': 'Petter'}]}
+
+
