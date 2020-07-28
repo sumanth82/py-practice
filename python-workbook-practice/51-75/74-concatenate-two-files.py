d5f42672c6ea3510938c1f2fe1c2bfed01fb9aa1
sumanth82 <sumanth82@gmail.com>
2020-07-28 05:05:22 -0400

    Updates

---
+++ python-workbook-practice/51-75/74-concatenate-two-files.py
@@ -0,0 +1,27 @@
+#Question: Please concatenate this file with this one to a single text file. The content of the output file should look like below.
+
+# https://www.pythonhow.com/data/sampledata.txt
+
+# https://pythonhow.com/data/sampledata_x_2.txt
+
+#Expected output: 
+
+#x,y
+#3,5
+#4,9
+#6,10
+#7,11
+#8,12
+#6,10
+#8,18
+#12,20
+#14,22
+#16,24
+
+import urllib3
+
+def fetch_files():
+    urllib.request.urlopen(url1)
+    
+
+
