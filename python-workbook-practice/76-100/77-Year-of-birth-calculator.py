dbfc1a0952f401313e878120c45917fdac849240
sumanth82 <sumanth82@gmail.com>
2020-08-04 01:47:15 -0400

    Updates

---
+++ python-workbook-practice/76-100/77-Year-of-birth-calculator.py
@@ -0,0 +1,11 @@
+#Exercise 77 - Year of Birth Calculator
+# Question: Create a script that asks the user to enter their age and the script calculates the user's year of birth and prints it out in a string like 
+# in the expected output. Please make sure you generate the current year dynamically.
+
+age = int(input('Enter your age: '))
+print(age)
+
+
+
+
+
