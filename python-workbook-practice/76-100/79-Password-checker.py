abdcd6ad9de78019026055015d19ff260206eb90
sumanth82 <sumanth82@gmail.com>
2020-08-11 08:26:59 -0400

    Updates

---
+++ python-workbook-practice/76-100/79-Password-checker.py
@@ -0,0 +1,37 @@
+# Create a script that lets the user submit a password until they have satisfied three conditions:
+
+#1. Password contains atleast one number
+#2. Contains one uppercase letter 
+#3. It is atleast 5 characters long 
+
+# Print message 'password is not fine' if the user didn't create a password.
+
+input_password = input('Enter a password with the prescribed requirement: ' )
+
+## Covert a String to a list:
+
+split_password=list(input_password)
+print(split_password)
+
+# O/P: ['t', 'e', 's', 't']
+
+counter=0
+
+for i in split_password:
+    if str.isdigit(split_password[i]) or str.isupper(split_password[i]):
+        print('All Good')
+    else:
+        print('Password is NOT fine')
+
+
+
+#if (str.isdigit(input_password))=True and (str.isupper(input_password))=True and len(input_password) > 5:
+#    print('Password is as expected')
+
+
+#for i in split_password:
+#    if i
+#if not int or not uppercase() or not len(5):
+#    print('Doesnt meet the requirements')
+#else:
+#    print(input_password)
