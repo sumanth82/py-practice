0940daecdb65d7ed132dae4928223dfe3f5143a6
sumanth82 <sumanth82@gmail.com>
2020-08-12 17:46:42 -0400

    Updates

--- python-workbook-practice/76-100/81-username-n-password-checker.py
+++ python-workbook-practice/76-100/81-username-n-password-checker.py
@@ -1,49 +1,45 @@
 # In addition to previous problem, here a list of users already exist in a file called 'users.txt'
 # Program should ask for a username and password. If the username already exists as in the 'users.txt' it should throw a message that the user already exists
 
-username = input('Enter a username: ')
+input_password = input('Enter a password: ')
 
-a=[]
+## Step-1: If UserName already exists in the file - users.txt then print the message and fail the program
 
 with open('users.txt', 'r', newline='\n') as userfile:
     b= (userfile.readlines())
     print(b)    # O/P: ['Mercury\n', 'Venus\n', 'Earth\n', 'Mars\n', 'Jupiter\n', 'Saturn\n', 'Uranus\n', 'Neptune\n', 'Pluto\n', 'Neptune']
     print(type(b)) # list 
 
-    #a = (userfile.read())
-    #print(a)
-    # O/P: Mercury
-    #Venus
-    #Earth
-    #Mars
-    #Jupiter
-    #Saturn
-    #Uranus
-    #Neptune
-    #Pluto
-    #Neptune
-    
-    #print(type(a))  # str
-
-    #input_list = list(a)
-    #print(input_list)
-
-# O/P: ['M', 'e', 'r', 'c', 'u', 'r', 'y', '\n', 'V', 'e', 'n', 'u', 's', '\n', 'E', 'a', 'r', 't', 'h', '\n', 'M', 'a', 'r', 's', '\n', 'J', 'u', 'p', 'i', 't', 'e', 'r', '\n', 'S', 'a', 't', 'u', 'r', 'n', '\n', 'U', 'r', 'a', 'n', 'u', 's', '\n', 'N', 'e', 'p', 't', 'u', 'n', 'e', '\n', 'P', 'l', 'u', 't', 'o', '\n', 'N', 'e', 'p', 't', 'u', 'n', 'e']
-
-
-#    b= (userfile.readlines())
-#    print(b)    # O/P: Empty List - []
-#    print(type(b)) # list 
-
-
-    for i in b:
-        if i == username+'\n':
-          print('Username already exists')
-          break
+    #for i in b:
+    #    if i == input_password+'\n':
+    #      print('Username already exists')
+    #      break
     
     # O/P: If user inputs the password as 'Pluto', output is as expected - Username already exists
     ### WORKS GREAT :-) 
 
+if any(i==input_password+'\n' for i in b):
+    print('Username already exists')
+    
+    
+    # O/P: If user inputs the password as 'Pluto', output is as expected - Username already exists
+    ### WORKS GREAT :-) 
 
+elif any(i.isdigit() for i in input_password) and any(i.isupper() for i in input_password) and len(input_password)>=5:
+    print('Password is fine')
+elif (any(i.isdigit() for i in input_password))!=True and (any(i.isupper() for i in input_password))!=True and len(input_password)>=5:
+    print('You need atleast one DIGIT and one UPPERCASE')
+elif any(i.isdigit() for i in input_password) and any(i.isupper() for i in input_password) and len(input_password)<5:
+    print('You must have atleast 5 letters')
+elif any(i.isupper() for i in input_password) and (any(i.isupper() for i in input_password))!=True and len(input_password)>=5:
+    print('you must have atleast one UPPERCASE')
+elif any(i.isdigit() for i in input_password) and (any(i.isupper() for i in input_password))!=True and len(input_password)<5:
+    print('you must have atleast one UPPERCASE and 5 letters')
+elif (any(i.isdigit() for i in input_password))!=True and any(i.isupper() for i in input_password) and len(input_password)<5:
+    print('you must have atleast one DIGIT and 5 letters')
+else:
+    print('Man - Out of options')
+
+### WORKS GREAT #####
 
 
