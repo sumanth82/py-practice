3a5dd76bd2c84e2a001fd783674539bdfe3d34fa
sumanth82 <sumanth82@gmail.com>
2020-08-15 11:32:39 -0400

    Updates

--- python-workbook-practice/76-100/85-data-cleaner.py
+++ python-workbook-practice/76-100/85-data-cleaner.py
@@ -19,3 +19,18 @@ with open('countries-raw.txt', 'w') as file:
 
 # O/P is as seen in file countries-raw.txt
 
+########## ALTERNATIVE SOLUTION ##########
+
+with open('countries_raw.txt', 'r') as file:
+    content=file.readlines()
+
+content = [i.strip('\n') for i in content if '\n' in i]
+content = [i for i in content if i!=""]
+content = [i for i in content if len(i)!=1]
+content=[i for i in content if i!="Top of Page"]
+
+with open('countries_clean.txt', 'w') as file: 
+    for i in content:
+        file.write(i+'\n')
+
+
