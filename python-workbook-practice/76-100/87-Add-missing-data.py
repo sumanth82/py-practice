3b526caacbfaaca5cbbbdd1fcba594ab7f5c3044
sumanth82 <sumanth82@gmail.com>
2020-08-16 08:07:45 -0400

    Updates

--- python-workbook-practice/76-100/87-Add-missing-data.py
+++ python-workbook-practice/76-100/87-Add-missing-data.py
@@ -26,3 +26,35 @@ with open('new_countries_missing.txt', 'w') as newfile:
 with open('countries_missing.txt', 'a') as newfile:         # 'a' - Used to Append
     newfile.write('\n')                                     # Append a newline
     newfile.writelines(newline_checklist)                   # This writes a list object into a single line, for each list element  
+
+
+############## ALTERNATIVE SOLUTION ##########
+
+
+checklist=["Portugal", "Germany", "Spain"]
+
+new_ardit_checklist=[]
+
+for i in checklist:
+    i+='\n'
+    new_ardit_checklist.append(i)
+
+print(new_ardit_checklist)
+
+with open('countries_missing.txt', 'r') as file:
+    content=file.readlines()
+
+updated_list=sorted(new_ardit_checklist+content)
+
+with open('countries_missing_fixed.txt', 'w') as file:
+    for i in updated_list:
+        file.write(i)
+
+
+
+
+
+
+
+
+
