c3e2f3d28716e2e9a6fb79fe0dbc0827d0f3b059
sumanth82 <sumanth82@gmail.com>
2020-08-25 19:15:55 -0400

    Updates

--- python-workbook-practice/76-100/94-url-cleaner.py
+++ python-workbook-practice/76-100/94-url-cleaner.py
@@ -13,3 +13,20 @@
 #http://www.stackoverflow.com
 #http://www.pythonhow.com
 
+url_entry=[]
+url_entry_modified=[]
+
+with open('urls.txt', 'r') as url_file:
+    for line in url_file:
+        url_entry.append(line)
+    print(url_entry)
+    #O/P: ['https:/www.google.com\n', 'https:/www.yahoo.com\n', 'https:/www.stackoverflow.com\n', 'https:/www.pythonhow.com\n']
+
+    for i in url_entry:
+        print(i)
+        print(type(i))
+        i = i.replace(r'https:/', r'http://')
+        print(i)
+
+
+
