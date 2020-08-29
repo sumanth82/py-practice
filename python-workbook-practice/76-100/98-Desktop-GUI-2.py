e9762da512dddcf4d9c8171112f5bdb19a5fc516
sumanth82 <sumanth82@gmail.com>
2020-08-29 15:51:40 -0400

    Updates

--- python-workbook-practice/76-100/98-Desktop-GUI-2.py
+++ python-workbook-practice/76-100/98-Desktop-GUI-2.py
@@ -12,12 +12,6 @@ from tkinter import *
 window=tkinter.Tk()
 window.title('First GUI app')
 
-window.rowconfigure(0, minsize=800, weight=1)
-window.columnconfigure(1, minsize=800, weight=1)
-
-border_effects={'raised': tkinter.RAISED}
-
-txt_edit=tkinter.Text(window)
 frame=tkinter.Frame(window)
 frame.pack()
 
